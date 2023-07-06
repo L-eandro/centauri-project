06851435a00caff742a2ca2da153f2018349bcda
Davidbjjj <60227838+Davidbjjj@users.noreply.github.com>
2023-07-06 00:15:21 -0300

    adicionando o spring boot

---
+++ src/main/java/com/example/Centaure/CentaureApplication.java
@@ -0,0 +1,13 @@
+package com.example.Centaure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentaureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentaureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Centaure/CentaureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Centaure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentaureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
