25448fb56d357a164237c91f5b78abb6053a568b
Davidbjjj <60227838+Davidbjjj@users.noreply.github.com>
2023-07-08 20:16:02 -0300

    adição

--- src/main/java/com/example/Centaure/CentauriApplication.java
+++ src/main/java/com/example/Centaure/CentauriApplication.java
@@ -7,20 +7,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 
 @SpringBootApplication
-<<<<<<< HEAD:src/main/java/com/example/Centaure/CentaureApplication.java
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
-public class CentaureApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CentaureApplication.class, args);
-
-		System.out.println("Vencemos, o código pegou, papai");
-=======
-public class CentauriApplication {
+	public class CentauriApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CentauriApplication.class, args);
->>>>>>> 1e8d1abe0068e185339b60e486562d44b635b11b:src/main/java/com/example/Centaure/CentauriApplication.java
-	}
 
-}
+		System.out.println("Vencemos, o código pegou, papai");}}
