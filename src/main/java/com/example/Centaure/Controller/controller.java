b5d724b0c70b12333819b95082230dd1a85f3a1e
Davidbjjj <60227838+Davidbjjj@users.noreply.github.com>
2023-07-09 20:07:16 -0300

    Adicionando a pasta Controllers e Models

--- src/main/java/com/example/Centaure/Controller/controller.java
+++ src/main/java/com/example/Centaure/Controller/controller.java
@@ -1,4 +1,4 @@
-package com.example.Centaure;
+package com.example.Centaure.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
