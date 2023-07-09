fff070c2c3c47a247241186c11cacb31857e1e48
Felipe Santana <felipe012santana012@gmail.com>
2023-07-09 02:30:04 -0300

    aprimorando e realizando implementações com java Script na pagina login

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,8 @@
+function exibirPassword() {
+    var passwordInput = document.getElementById("password");
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+    } else {
+      passwordInput.type = "password";
+    }
+  }
