1607ea7c19b3f2f75a40a88b2d3ed23a4c5f265d
Leandro José <leandrojose.a98@gmail.com>
2023-07-18 21:58:10 -0300

    asd

--- src/main/java/com/example/centaure/Centaure/controllers/UsuarioController.java
+++ src/main/java/com/example/centaure/Centaure/controllers/UsuarioController.java
@@ -6,7 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class UsuarioController {
@@ -29,6 +32,12 @@ public class UsuarioController {
     public String listar(Model model, Usuario usuario ){
         model.addAttribute("usuario", this.usuarioServive.listar(usuario));
         return "listar_usuario";
+    }
+    @GetMapping("/usuario/editar/{id}")
+    public String editar(@PathVariable Integer id, Model model){
+        Optional<Usuario> usuario = this.usuarioServive.editar(id);
+        model.addAttribute("usuario", usuario);
+        return "usuario_editar";
     }
 
 
--- src/main/java/com/example/centaure/Centaure/service/UsuarioServive.java
+++ src/main/java/com/example/centaure/Centaure/service/UsuarioServive.java
@@ -4,9 +4,12 @@ import com.example.centaure.Centaure.models.Usuario;
 import com.example.centaure.Centaure.repositores.UsuarioRepositores;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
 
 @Service
 public class UsuarioServive {
@@ -20,6 +23,9 @@ public class UsuarioServive {
 
     public List<Usuario> listar(Usuario usuario){
         return usuarioRepositores.findAll();
-    } 
+    }
+    public Optional<Usuario> editar(Integer id){
+        return usuarioRepositores.findById(id);
+    }
 
 }
