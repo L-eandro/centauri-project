ac352f08647f228aeb38271dbf14d54bf551bc90
Leandro José <leandrojose.a98@gmail.com>
2023-07-26 23:34:26 -0300

    :sparkles: Adicionando validação de login motorista
    e alguma modificações nele e em cadastro

--- src/main/java/com/example/centaure/Centaure/controllers/MotoristaController.java
+++ src/main/java/com/example/centaure/Centaure/controllers/MotoristaController.java
@@ -4,12 +4,14 @@ import com.example.centaure.Centaure.models.Motorista;
 import com.example.centaure.Centaure.models.Usuario;
 import com.example.centaure.Centaure.service.MotoristaService;
 import extencao.MotoristaInvalid;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
@@ -18,6 +20,39 @@ import java.util.Optional;
 public class MotoristaController {
     @Autowired
     private MotoristaService motoristaService;
+
+    @GetMapping("/login/motorista")
+    public String Login(){
+        return "motorista_html/login_motorista";
+    }
+
+    @PostMapping("/login/motorista")
+    public String login(@RequestParam String email, @RequestParam String senha, HttpSession session, RedirectAttributes re){
+        Motorista motorista = motoristaService.login(email,senha);
+        if(motorista != null){
+            session.setAttribute("logado", motorista);
+            re.addFlashAttribute("msg2", "Logado com sucesso!");
+            return "redirect:/login/motorista";
+        } else {
+            re.addFlashAttribute("msg", "login ou senha incorretas!");
+            return "redirect:/login/motorista";
+        }
+    }
+
+    @GetMapping("/esqueci-senha")
+    public String esqueciSenha(){
+        return "esqueci_senha";
+    }
+
+
+
+
+
+
+    @GetMapping("/")
+    public String Index(){
+        return "index";
+    }
 
     @GetMapping("/cadastro/motorista")
     public String cadastroMotorista() {
@@ -29,7 +64,7 @@ public class MotoristaController {
         try {
             motoristaService.salvar(motorista);
             motoristaService.criar(motorista);
-            ra.addFlashAttribute("msgSucess", "Conta criada com socesso!");
+            ra.addFlashAttribute("msgSucess", "Conta criada com sucesso!");
             return "redirect:/cadastro/motorista";
 
         } catch (MotoristaInvalid e ){
