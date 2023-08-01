8ef45f770d95bf12bfe302a429bf7b5042122ca9
Gabriel C <127120216+Gabriel00261@users.noreply.github.com>
2023-08-01 12:21:20 -0300

    coment

--- src/main/java/com/example/centaure/Centaure/service/VeiculoService.java
+++ src/main/java/com/example/centaure/Centaure/service/VeiculoService.java
@@ -2,11 +2,9 @@ package com.example.centaure.Centaure.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.centaure.Centaure.models.Usuario;
 import com.example.centaure.Centaure.models.Veiculo;
 import com.example.centaure.Centaure.repositores.VeiculoRepositores;
-
 import extencao.UserInvalid;
 import extencao.VeiculoInvalid;
 
@@ -17,12 +15,14 @@ public class VeiculoService {
      @Autowired
     private VeiculoRepositores veiculoRepositores;
 
+    // Método para criar um novo veículo.
       public Veiculo criar(Veiculo veiculo) {
-        return veiculoRepositores.save(veiculo);
+        return veiculoRepositores.save(veiculo); // Salva o novo veículo no banco de dados.
 
-    }
+      }
 
-     public void salvando(Veiculo veiculo) throws VeiculoInvalid {
+    // Método para validar e salvar um veículo, lançando exceções personalizadas em caso de campos obrigatórios vazios.
+    public void salvando(Veiculo veiculo) throws VeiculoInvalid {
         if (veiculo.getDocumento().trim().isEmpty() || veiculo.getAnoFabricacao().trim().isEmpty()
                 || veiculo.getCapacidade().trim().isEmpty() || veiculo.getCor().trim().isEmpty()
                 || veiculo.getNomeModelo().trim().isEmpty() || veiculo.getPlaca().trim().isEmpty()) {
--- src/main/java/extencao/VeiculoInvalid.java
+++ src/main/java/extencao/VeiculoInvalid.java
@@ -7,6 +7,7 @@ public class VeiculoInvalid extends Exception{
      @Serial
     private static final long serialVersionUID = 1L;
 
+    // Construtor da exceção UserInvalid, que recebe uma mensagem como argumento.
     public VeiculoInvalid(String msg) {
         super(msg);
     }
