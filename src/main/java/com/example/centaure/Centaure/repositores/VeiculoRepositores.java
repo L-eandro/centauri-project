b636f83892d423e249a1ae83414f3a2689e67f15
Gabriel C <127120216+Gabriel00261@users.noreply.github.com>
2023-08-04 11:08:30 -0300

    serv/repo veículo

--- src/main/java/com/example/centaure/Centaure/repositores/VeiculoRepositores.java
+++ src/main/java/com/example/centaure/Centaure/repositores/VeiculoRepositores.java
@@ -3,8 +3,18 @@ package com.example.centaure.Centaure.repositores;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import com.example.centaure.Centaure.models.Veiculo;
+import java.util.Optional;
+
 
 @Component
 public interface VeiculoRepositores extends JpaRepository<Veiculo, Integer> {
-    
+
+    // Método para procurar um veículo pelo número do documento
+    public Optional<Veiculo> findBydocumento(String documento);
+
+    // Métodos para verificar a existência de um veículo pelo número do documento, pela placa e pelo nome do modelo
+    boolean existsByDocumento(String documento);
+    boolean existsByPlaca(String placa);
+    boolean existsByNomeModelo(String nomeModelo);
+
 }
--- src/main/java/com/example/centaure/Centaure/service/VeiculoService.java
+++ src/main/java/com/example/centaure/Centaure/service/VeiculoService.java
@@ -8,6 +8,9 @@ import com.example.centaure.Centaure.repositores.VeiculoRepositores;
 import extencao.UserInvalid;
 import extencao.VeiculoInvalid;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class VeiculoService {
@@ -28,7 +31,44 @@ public class VeiculoService {
                 || veiculo.getNomeModelo().trim().isEmpty() || veiculo.getPlaca().trim().isEmpty()) {
             throw new VeiculoInvalid("Os campos obrigatórios não podem estar vazio.");
         }
+        if (veiculoRepositores.existsByDocumento(veiculo.getDocumento())) {
+            throw new VeiculoInvalid("Veículo com o número de documento já cadastrado.");
+        }
+        if (veiculoRepositores.existsByPlaca(veiculo.getPlaca())) {
+            throw new VeiculoInvalid("Veículo com a placa já cadastrada.");
+        }
+        if (veiculoRepositores.existsByNomeModelo(veiculo.getNomeModelo())) {
+            throw new VeiculoInvalid("Veículo com o nome do modelo já cadastrado.");
+        }
 
     }
+
+    public Optional<Veiculo> findBydocumento(String documento) {
+        return veiculoRepositores.findBydocumento(documento);
+    }
+
+    public List<Veiculo> listar() {
+        return veiculoRepositores.findAll();
+    }
+
+    public Optional<Veiculo> buscarPorId(Integer id) {
+        return veiculoRepositores.findById(id);
+    }
+
+    public Veiculo cadastrar(Veiculo veiculo) {
+        return veiculoRepositores.save(veiculo);
+    }
+
+    public Veiculo editar(Veiculo veiculo) {
+        return veiculoRepositores.save(veiculo);
+    }
+
+    public String deletar(Integer id){
+        veiculoRepositores.deleteById(id);
+        return "";
+    }
+
+
+
 
 }
